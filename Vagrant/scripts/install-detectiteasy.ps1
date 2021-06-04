02480f6b00975408c614fcca9890d07a24411d84
Jonathan Echavarria <jechavarria@reliaquest.com>
2021-06-04 15:37:07 -0400

    updated die shourtcut

--- Vagrant/scripts/install-detectiteasy.ps1
+++ Vagrant/scripts/install-detectiteasy.ps1
@@ -4,5 +4,6 @@ Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Installing Detect It Easy via chocola
 
 Write-Host "Installing Detect It Easy..."
 choco install -y --limit-output --no-progress die 
+Install-ChocolateyShortcut -ShortcutFilePath "C:\Users\Vagrant\Desktop\DetectItEasy.lnk" -TargetPath "C:\ProgramData\chocolatey\bin\die.exe"
 
 Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Detect It Easy install complete!"
