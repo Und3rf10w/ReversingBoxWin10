a8606dc3bee4fec2118be58d39c9bea667cc388a
Jonathan Echavarria <jechavarria@reliaquest.com>
2021-06-04 15:39:10 -0400

    pebear shortcuts

--- Vagrant/scripts/install-pebear.ps1
+++ Vagrant/scripts/install-pebear.ps1
@@ -4,5 +4,6 @@ Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Installing pebear via chocolatey..."
 
 Write-Host "Installing pebear..."
 choco install -y --limit-output --no-progress pebear
+Install-ChocolateyShortcut -ShortcutFilePath "C:\Users\Vagrant\Desktop\PEbear.lnk" -TargetPath "C:\ProgramData\chocolatey\bin\PE-bear.exe"
 
 Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Pebear install complete!"
