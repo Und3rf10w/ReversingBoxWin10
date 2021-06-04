79bbd97822513341e1005c79ddb049eb37605c5e
Jonathan Echavarria <jechavarria@reliaquest.com>
2021-06-04 15:36:07 -0400

    added ghidra shourtcuts

--- Vagrant/scripts/install-ghidra.ps1
+++ Vagrant/scripts/install-ghidra.ps1
@@ -4,5 +4,6 @@ Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Installing ghidra via chocolatey..."
 
 Write-Host "Installing ghidra..."
 choco install -y --limit-output --no-progress ghidra
+copy-item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Ghidra.lnk" -Destination "C:\Users\Vagrant\Desktop\Ghidra.lnk"
 
 Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Ghidra install complete!"
