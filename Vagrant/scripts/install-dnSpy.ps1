ce455a4c4922cc07503f91f7f3b35a5133f6d819
Jonathan Echavarria <jechavarria@reliaquest.com>
2021-06-04 15:38:21 -0400

    dnspy shourtcut

--- Vagrant/scripts/install-dnSpy.ps1
+++ Vagrant/scripts/install-dnSpy.ps1
@@ -4,5 +4,6 @@ Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Installing dnSpy via chocolatey..."
 
 Write-Host "Installing dnSpy..."
 choco install -y --limit-output --no-progress dnSpy
+Install-ChocolateyShortcut -ShortcutFilePath "C:\Users\Vagrant\Desktop\dnSpy.lnk" -TargetPath "C:\ProgramData\chocolatey\bin\dnSpy.exe"
 
 Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) dnSpy install complete!"
