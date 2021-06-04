87e1e643220f6a6284edaf29701d4144e3132187
Jonathan Echavarria <jechavarria@reliaquest.com>
2021-06-04 15:39:38 -0400

    pestudio shortcuts

--- Vagrant/scripts/install-pestudio.ps1
+++ Vagrant/scripts/install-pestudio.ps1
@@ -4,5 +4,6 @@ Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Installing pestudio via chocolatey...
 
 Write-Host "Installing pestudio..."
 choco install -y --limit-output --no-progress pestudio 
+Install-ChocolateyShortcut -ShortcutFilePath "C:\Users\Vagrant\Desktop\pestudio.lnk" -TargetPath "C:\ProgramData\chocolatey\bin\pestudio.exe"
 
 Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) pestudio install complete!"
