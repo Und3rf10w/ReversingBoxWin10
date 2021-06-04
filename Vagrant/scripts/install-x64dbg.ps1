c6c103fe9d2e9ff3ae1f7dc23ea72f4b87f2a9be
Jonathan Echavarria <jechavarria@reliaquest.com>
2021-06-04 15:40:29 -0400

    x64dbg shortcuts

--- Vagrant/scripts/install-x64dbg.ps1
+++ Vagrant/scripts/install-x64dbg.ps1
@@ -4,5 +4,7 @@ Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Installing x64dbg via chocolatey..."
 
 Write-Host "Installing x64dbg..."
 choco install -y --limit-output --no-progress x64dbg.portable
+Install-ChocolateyShortcut -ShortcutFilePath "C:\Users\Vagrant\Desktop\x32dbg.lnk" -TargetPath "C:\ProgramData\chocolatey\bin\x32dbg.exe"
+Install-ChocolateyShortcut -ShortcutFilePath "C:\Users\Vagrant\Desktop\x64dbg.lnk" -TargetPath "C:\ProgramData\chocolatey\bin\x64dbg.exe"
 
 Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) x64dbg install complete!"
